a14d27e0c5207ab0f135f7a8e05f5ad3bbc0bde6
danalmeidap <dbatistadealmeida23@gmail.com>
2022-11-08 18:55:22 -0300

    Response added on server.py

--- server.py
+++ server.py
@@ -1,8 +1,12 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends,Response,status
+from typing import List
 from src.schemas.schemas import Product
 from src.schemas.schemas import User
+from src.schemas.schemas import Order
 from src.infra.sqlalchemy.repositories.product import ProductRepository
 from src.infra.sqlalchemy.repositories.user import UserRepository
+from src.infra.sqlalchemy.repositories.order import OrderRepository
+from src.infra.sqlalchemy.models import models
 from sqlalchemy.orm import Session
 from src.infra.sqlalchemy.config.database import get_db, create_db
 
@@ -10,45 +14,100 @@ app = FastAPI()
 create_db()
 
 @app.get("/products")
-def products_list(db:Session=Depends(get_db)):
+async def products_list(response:Response, db:Session=Depends(get_db)):
     products = ProductRepository(db).product_list()
     return products
 
 @app.post("/products")
-def create_product(product: Product, db: Session = Depends(get_db)):
+async def create_product(product: Product, response:Response, db: Session = Depends(get_db)):
     created_product = ProductRepository(db).create(product)
+    response.status_code = status.HTTP_201_CREATED
     return created_product
 
 @app.get("/products/{product_id}")
-def get_product(product_id:int, db: Session = Depends(get_db)):
+async def get_product(product_id:int, response:Response, db: Session = Depends(get_db)):
     product = ProductRepository(db).get_product(product_id)
-    return product
+    if not product:
+        response.status_code = status.HTTP_404_NOT_FOUND
+    return product if product else response
 
 @app.delete("/products/{product_id}")
-def delete_product(product_id:int, db:Session = Depends(get_db)):
-    deleted_product = ProductRepository(db).remove(product_id)
-    if deleted_product:
-        return {"Msg": "Product Deleted"}
-    return {"Msg":"You don't have products with this id"}    
+async  def delete_product(product_id:int, response:Response, db:Session = Depends(get_db)):
+    product = ProductRepository(db).get_product(product_id)
+    if not product:
+        response.status_code= status.HTTP_404_NOT_FOUND
+    else:
+        if ProductRepository(db).remove(product_id):
+            response.status_code = status.HTTP_204_NO_CONTENT
+        return (
+            f"Product id {product_id} deleted"
+            if response.status_code == status.HTTP_204_NO_CONTENT
+            else response
+        )
+
+
 
 @app.get("/users")
 def users_list(db:Session=Depends(get_db)):
     users = UserRepository(db).users_list()
     return users
 
+
 @app.post("/users")
-def create_user(user: User, db:Session= Depends(get_db)):
+def create_user(user: User, response:Response,  db:Session= Depends(get_db)):
     created_user = UserRepository(db).create(user)
+    response.status_code = status.HTTP_200_OK
     return created_user
 
+
 @app.get("/users/{user_id}")
-def get_user(user_id:int, db:Session= Depends(get_db)):
+def get_user(user_id:int, response:Response, db:Session= Depends(get_db)):
     user = UserRepository(db).get_user(user_id)
-    return user
+    if not user:
+        response.status_code = status.HTTP_404_NOT_FOUND
+    return user if user else response
 
 @app.delete("/users/{user_id}")
-def delete_user(user_id:int, db:Session= Depends(get_db)):
-    deleted_user= UserRepository(db).remove(user_id)
-    if deleted_user:
-        return {"msg": "User deleted"}
-    return {"msg":"You don't have users with this id"}    
+def delete_user(user_id:int, response:Response, db:Session= Depends(get_db)):
+    user = UserRepository(db).get_user(user_id)
+    if not user:
+        response.status_code = status.HTTP_404_NOT_FOUND
+    else:
+        if UserRepository(db).remove(user_id):
+            response.status_code = status.HTTP_204_NO_CONTENT
+        return (
+            f"User id {user_id} deleted"
+            if response.status_code == status.HTTP_204_NO_CONTENT
+            else response
+        )
+
+@app.get("/orders")
+async def orders_list(db:Session= Depends(get_db)):
+    return OrderRepository(db).orders_list()
+
+@app.post("/orders")
+async def create_order(order: Order, response: Response, db:Session= Depends(get_db)):
+    created_order = OrderRepository(db).create(order)
+    response.status_code = status.HTTP_201_CREATED
+    return created_order
+
+@app.get("/orders/{order_id}")
+async def get_order(order_id:int, response: Response, db:Session= Depends(get_db)):
+    order = OrderRepository(db).get_order(order_id)
+    if not order:
+         response.status_code = status.HTTP_404_NOT_FOUND
+    return order if order else response     
+
+@app.delete("/orders/{user_id}")
+async def delete_order(order_id:int, response: Response, db:Session= Depends(get_db)):
+    order = OrderRepository(db).get_order(order_id)
+    if not order:
+        response.status_code = status.HTTP_404_NOT_FOUND
+    else:
+        if OrderRepository.remove(order_id):
+            response.status_code = status.HTTP_204_NO_CONTENT
+        return (
+            f"Order id {order_id} deleted"
+            if response.status_code == status.HTTP_204_NO_CONTENT
+            else response
+        )    
