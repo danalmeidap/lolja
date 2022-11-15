13e180b4cbdab9923411ee377c9756fbf2acfac2
danalmeidap <dbatistadealmeida23@gmail.com>
2022-11-15 16:24:31 -0300

    Organizing outputs on schemas.py

--- src/schemas/schemas.py
+++ src/schemas/schemas.py
@@ -3,28 +3,6 @@ from typing import List, Optional
 from pydantic import BaseModel
 
 
-class UserOut(BaseModel):
-    id: Optional[int] = None
-    name: str
-    phonee: str
-
-    class Config:
-        orm_mode = True
-
-
-class Product(BaseModel):
-    id: Optional[int] = None
-    name: str
-    details: str
-    price: float
-    avaiable: bool = False
-    user_id: Optional[int]
-    user: Optional[UserOut]
-
-    class Config:
-        orm_mode = True
-
-
 class ProductOut(BaseModel):
     id: Optional[str] = None
     name: str
@@ -40,6 +18,29 @@ class User(BaseModel):
     phonee: str
     password: str
     products: List[ProductOut] = []
+   
+
+    class Config:
+        orm_mode = True
+
+
+class UserOut(BaseModel):
+    id: Optional[int] = None
+    name: str
+    phonee: str
+    
+    class Config:
+        orm_mode = True
+
+
+class Product(BaseModel):
+    id: Optional[int] = None
+    name: str
+    details: str
+    price: float
+    avaiable: bool = False
+    user_id: Optional[int]
+    user: Optional[UserOut]
 
     class Config:
         orm_mode = True
